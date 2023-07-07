5803cee414750d92eb7e4ae96723a713d8386369
kingpotato <1002638696@qq.com>
2023-07-07 15:16:33 +0800

    modified:   raytracer/src/main.rs 	modified:   raytracer/src/material.rs

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -56,7 +56,7 @@ fn ray_color(r: Ray, world: &mut HittableList, depth: i32) -> Color {
 }
 fn main() {
     //
-    let path = std::path::Path::new("output/book1/image14.jpg");
+    let path = std::path::Path::new("output/book1/image15.jpg");
     let prefix = path.parent().unwrap();
     std::fs::create_dir_all(prefix).expect("Cannot create all the parents");
     //Image
@@ -105,7 +105,14 @@ fn main() {
             e: (-1.0, 0.0, -1.0),
         },
         radius: 0.5,
-        mat_ptr: material_left,
+        mat_ptr: material_left.clone(),
+    }));
+    world.add(Box::new(Sphere {
+        center: Point3 {
+            e: (-1.0, 0.0, -1.0),
+        },
+        radius: -0.4,
+        mat_ptr: material_left.clone(),
     }));
     world.add(Box::new(Sphere {
         center: Point3 {
--- raytracer/src/material.rs
+++ raytracer/src/material.rs
@@ -1,4 +1,4 @@
-use crate::randoms::{min, random_in_unit_sphere, random_unit_vec};
+use crate::randoms::{min, random_double, random_in_unit_sphere, random_unit_vec};
 use crate::vec3::{mul_vec_dot, reflect, refract, Color, Vec3};
 
 use crate::{hittable::HitRecord, ray::Ray};
@@ -59,6 +59,11 @@ impl Material for Metal {
 pub struct Dielectric {
     pub ir: f64,
 }
+
+pub fn reflectance(cosine: f64, ref_idx: f64) -> f64 {
+    let r0 = (1.0 - ref_idx) / (1.0 + ref_idx);
+    r0 * r0 + (1.0 - r0 * r0) * ((1.0 - cosine).powf(5.0))
+}
 impl Material for Dielectric {
     fn scatter(
         &self,
@@ -77,7 +82,9 @@ impl Material for Dielectric {
         let cos_theta = min(mul_vec_dot(-unit_direction, rec.normal), 1.0);
         let sin_theta = (1.0 - cos_theta * cos_theta).sqrt();
         let cannot_refract = reflection_ratio * sin_theta > 1.0;
-        let direction = if cannot_refract {
+        let direction = if cannot_refract
+            || reflectance(cos_theta, reflection_ratio) > random_double(0.0, 1.0)
+        {
             reflect(unit_direction, rec.normal)
         } else {
             refract(unit_direction, rec.normal, reflection_ratio)
